3fd73bf6bc770f4aa7ab1b616b5fcf4e0e020cea
sukaynaghnemat <sukaynaghnemat@yahoo.com>
2021-02-08 22:34:53 +0200

    new change

---
+++ js/app.js
@@ -0,0 +1,95 @@
+'use strict';
+
+
+let username=prompt("whats your name?");
+alert("Hello"+" ;"+ username + ";" +"please answer a questions");
+console.log(username);
+
+
+
+
+
+let f1 =prompt(username+ " ;" +"do you think i like football?");
+
+if (f1.toLocaleLowerCase() === "yes"|| f1.toLocaleLowerCase() === "y" )
+{
+    alert("yes it is correct");
+    console.log("it is correct");
+}
+
+
+else {
+    alert("no it is rong");
+    console.log("it is rong");
+
+}
+
+let f2 =prompt(username+ "-" +"do you think i love real madrid ?");
+
+if (f2.toLocaleLowerCase() === "yes"|| f2.toLocaleLowerCase() === "y" )
+{
+    alert("yes it is correct");
+    console.log("it is correct");
+    
+}
+else{
+    alert("no it is rong");
+    console.log("it is rong");
+}
+
+let f3 =prompt(username+ "-"+ "do you think i watch football match?");
+
+if (f3.toLocaleLowerCase() === "yes"|| f3.toLocaleLowerCase() === "y" )
+{
+    alert("yes it is correct");
+    console.log("it is correct");
+    
+    
+}
+else {
+    alert("no it is rong");
+    console.log("it is rong");
+
+
+}
+
+let f4 =prompt(username+ "-"+ "do you i play football?");
+
+
+if (f4.toLocaleLowerCase() === "no"|| f4.toLocaleLowerCase() === "n" )
+{
+    alert("yes it is correct");
+    console.log("it is correct");
+    
+}
+else{
+
+    alert("no it is rong");
+    console.log("it is rong");
+}
+
+let f5 =prompt(username+"-"+"do you think i visit santiago bernabeu?");
+
+if (f5.toLocaleLowerCase() === "no"|| f5.toLocaleLowerCase() === "n" )
+{
+    alert("yes it is correct");
+    console.log("it is correct");
+    
+}
+else{
+    alert("no it is rong");
+    console.log("it is rong");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
