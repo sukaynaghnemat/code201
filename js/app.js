b088a40b5d1588760ea13b9942ee84fcf2118c5e
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-02-10 15:24:48 +0200

    work in sukayna repo

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 'use strict';
 
-
-let username=prompt("whats your name?");
-alert("Hello"+" ;"+ username + ";" +"please answer a questions");
+function userInfo ( username , userPhone){
+ username=prompt('whats your name?');
+alert('Hello '+ username );
 //console.log(username);
+ userPhone= prompt('What\'s your phone number ?');
+alert ('I will be contact with you soon ')
+}
+userInfo ();
 
-
-
+alert ('please answer the following questions');
 let w = 0;
-
-let f1 =prompt(username+ " ;" +"do you think i like football?");
+function yesNoQ ( f1, f2,f3,f4,f5)
+{
+ f1 =prompt('Do you think i like football?');
 
 if (f1.toLocaleLowerCase() === "yes"|| f1.toLocaleLowerCase() === "y" )
 {
@@ -26,9 +30,9 @@ else {
 
 }
 
-let f2 =prompt(username+ "-" +"do you think i love real madrid ?");
+ f2 =prompt("Do you think i love real madrid ?");
 
-if (f2.toLocaleUpperCase() === "yes"|| f2.toLocaleUpperCase() === "y" )
+if (f2.toLocaleLowerCase() === "yes"|| f2.toLocaleLowerCase() === "y" )
 {
     alert("yes it is correct");
     //console.log("it is correct");
@@ -40,7 +44,7 @@ else{
     //console.log("it is rong");
 }
 
-let f3 =prompt(username+ "-"+ "do you think i watch football match?");
+f3 =prompt('Do you think i watch football match?');
 
 if (f3.toLocaleLowerCase() === "yes"|| f3.toLocaleLowerCase() === "y" )
 {
@@ -57,7 +61,7 @@ else {
 
 }
 
-let f4 =prompt(username+ "-"+ "do you think i play football?");
+ f4 =prompt('Do you think i play football?');
 
 
 if (f4.toLocaleLowerCase() === "no"|| f4.toLocaleLowerCase() === "n" )
@@ -73,7 +77,7 @@ else{
     //console.log("it is rong");
 }
 
-let f5 =prompt(username+"-"+"do you think i visit santiago bernabeu?");
+ f5 =prompt('Do you think i visit santiago bernabeu?');
 
 if (f5.toLocaleLowerCase() === "no"|| f5.toLocaleLowerCase() === "n" )
 {
@@ -86,14 +90,15 @@ else{
     alert("no it is rong");
     //console.log("it is rong");
 }
+}
+yesNoQ();
 
-
-
+function guessNum (prompt6){
 let myAge = 34;
         let j = 0;
         while (j <= 4) {
             j++;
-            let prompt6 = parseInt(prompt('What\s my age?'));
+             prompt6 = parseInt(prompt('What\s my age?'));
             if (j > 4) {
                 alert('Let\'s move on');
             } else if (prompt6 === myAge) {
@@ -110,40 +115,51 @@ let myAge = 34;
                 alert('your answer invalid');
         }
     }
+}
+guessNum();
 
 
+function guessItem(f7 ){
 
+ let fav=['flower','sea','birds'];
+        let y ;
 
+        for(y = 0 ; y < 6 ; y++ )
+        {
+            f7=prompt("what you believe i prefer? ");
+            f7=f7.toLocaleLowerCase();
 
-    let fav=['flower','sea','birds'];
+            for(let x= 0 ; x < fav.length; x++)
+            {    
+        if(f7 === fav[x])
+        {
 
-   
-
-        
-        let y=0;
-
-        for(let x = 0 ; x < fav.length ; x++ ){
-
-            for( y = 0 ; y < 6 ; y++){     
-
-        let f7=prompt("what you believe i prefer? ");
-
-        if(f7 == 'flower'|| f7 =='sea' || f7 =='birds'){
 
             alert("Thats correct");
             w++;
+            y =8;
             break;
 
         }
-        else{
-            alert("Try again!");
-        } 
 
         }
        
-        
-    }
-    document.write("total score:" + " "+ w);
+    }   
+    
+}
+guessItem();
+
+    alert("your total score: " + w +'out of 7');
+let grade;
+function totalGrade( w )
+{
+     grade= (w * 100) /7;
+    return grade
+
+}
+totalGrade(w);
+alert( ' your total grade is'+ grade +'%');
+
 
 
   
